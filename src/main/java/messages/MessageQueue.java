5b48f92f93400810738ce2b8f1e7faeb2efb37df
O.Bilenko <oleksandr.bilenko@shopgate.com>
2021-05-12 10:19:52 +0200

    Use SynchronousQueue for 1:1 communication

--- src/main/java/messages/MessageQueue.java
+++ src/main/java/messages/MessageQueue.java
@@ -1,8 +1,8 @@
 package messages;
 
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 
 /**
  * Messages blocking queue to transfer only {@code Message} objects
  */
-public class MessageQueue extends LinkedBlockingQueue<Message> {}
+public class MessageQueue extends SynchronousQueue<Message> {}
